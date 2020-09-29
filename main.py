2dd64bf3566b24aadbc70bed5bc2afb6f0911829
miguelboing <71512482+miguelboing@users.noreply.github.com>
2020-09-28 23:16:44 -0300

    Criação do Menu inicial

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#commit para testar, mas também ja pode ser nosso main file.
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Criando as classes para cada uma das telas
+class Gerenciador(ScreenManager):
+    pass
+
+class Menu(Screen):
+    pass
+
+class Interface_G(Screen):
+    pass
+
+class Info(Screen):
+    pass 
+
+# Classe com o método para rodar o App
+class Simulador(App):
+    def build(self):
+        return Gerenciador()
+
+# Rodando o programa
+if __name__ == '__main__':
+    Simulador().run()
+
