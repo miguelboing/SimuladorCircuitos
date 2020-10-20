75237250b14bdad5c125ac3df12eec1071622116
miguelboing <71512482+miguelboing@users.noreply.github.com>
2020-10-19 21:32:44 -0300

    Adding functionality to the component buttons. Creating a custom widget for the component with a drag behavior.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#import components
+
 from kivy.app import App
+import kivy
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout 
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.widget import Widget
+from kivy.uix.behaviors import DragBehavior
+
+kivy.require('1.11.1')
 
 # Criando as classes para cada uma das telas
 class Gerenciador(ScreenManager):
@@ -13,10 +20,14 @@ class Menu(Screen):
     pass
 
 class Interface_G(Screen):
-    pass
-
+    def addComponent(self):
+        self.ids.workflow.add_widget(Comp()) # Método para criar componentes
 class Info(Screen):
     pass 
+
+class Comp(DragBehavior, Widget): #Classe contendo atributos dos componentes
+    pass
+
 
 # Classe com o método para rodar o App
 class Simulador(App):
